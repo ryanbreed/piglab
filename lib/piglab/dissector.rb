7120fd93eacce0b9fc46bdbba3e71d1575d1da21
Ryan Breed <opensource@breed.org>
2017-01-29 14:33:25 -0600

    still not sure about this api

--- lib/piglab/dissector.rb
+++ lib/piglab/dissector.rb
@@ -1,28 +1,33 @@
 module Piglab
   class Dissector
     include Piglab::Regexes
-    attr_accessor :samples, :patterns
+    attr_accessor :samples, :patterns, :sample_glob, :grok_glob
     def initialize(*args)
       Hash[*args].each {|k,v| self.send(format('%s=',k),v)}
-      @samples ||= []
-      @patterns ||= []
+      @sample_glob = 'extracts/text/*.txt'
+      @grok_glob   = 'patterns/*.grok'
+      @samples ||= read_samples
+      @patterns ||= read_groks
       yield self if block_given?
       self
     end
-    def read_samples(glob='extracts/text/*.txt')
-      Dir.glob(glob).map {|f| File.read(f) }
+    def read_samples(glob=sample_glob)
+      Dir.glob(glob).map {|f| File.read(f,nil,encoding: Encoding::BINARY) }
     end
-    def read_groks(filename)
-      File.readlines(filename).map do |line|
-        line.chomp
-        name,pattern = line.split(%r{\s+},2)
-        g=Grok.new
-        g.add_pattern(name,pattern)
-        g
+    def read_groks(glob=grok_glob)
+      Dir.glob(glob).flat_map do |filename|
+        File.readlines(filename).map do |line|
+          line.chomp!
+          name,pattern = line.split(%r{\s+},2)
+          g=Grok.new
+          g.add_pattern(name,pattern)
+          g.compile("%{#{name}}")
+          g
+        end
       end
     end
     def matchers
-      patterns.map do |pattern|
+      @matchers ||= patterns.map do |pattern|
         case pattern
         when String
           Regexp.new(pattern)
@@ -36,16 +41,26 @@ module Piglab
       end.reject(&:nil?)
     end
     def sample_strings
-      samples.map(&:to_s)
+      @sample_strings ||= samples.map(&:to_s)
     end
-    def to_byte_match(str)
-      format('|%s|', str.bytes.map {|b| b.to_s(16)}.join(" "))
+    def to_byte_match(subj,end_idx=-1)
+      str=case subj
+      when String
+        subj
+      when Grok::Match
+        subj.match[0]
+      when MatchData
+        subj.to_s
+      else
+        ""
+      end
+      format('|%s|', str[0..end_idx].bytes.map {|b| b.to_s(16)}.join(" "))
     end
-    def longest_matcher(str, patterns=[])
-      lengths=Hash[patterns.zip(patterns.map {|p| p.match(str)&.size })]
-                .reject {|k,v| v.nil?}
-      pattern, size = lengths.max_by {|k,v| v }
-      pattern.match(str)
+    def longest_match(str)
+      valid=Hash[matchers.zip(matchers.map {|p| p.match(str) })]
+                .reject {|k,v| v.nil? || v==false }
+      grok,grokmatch = valid.max_by {|k,v| v.end - v.start }
+      grokmatch
     end
   end
 end
