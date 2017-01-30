05adb842a2b5c183a0561d347312729a0a8427c0
Ryan Breed <opensource@breed.org>
2017-01-29 22:04:24 -0600

    dissector grokpile

--- lib/piglab/dissector.rb
+++ lib/piglab/dissector.rb
@@ -1,44 +1,33 @@
 module Piglab
   class Dissector
     include Piglab::Regexes
-    attr_accessor :samples, :patterns, :sample_glob, :grok_glob
+    attr_accessor :samples, :sample_glob, :grok_glob
+    attr_accessor :patterns
+    attr_reader   :pile
     def initialize(*args)
       Hash[*args].each {|k,v| self.send(format('%s=',k),v)}
-      @sample_glob = 'extracts/text/*.txt'
-      @grok_glob   = 'patterns/*.grok'
-      @samples ||= read_samples
-      @patterns ||= read_groks
+      @sample_glob =   'extracts/text/*.txt'
+      @grok_glob   =   'patterns/*.grok'
+      @samples     ||= read_samples(sample_glob)
+      @pile        ||= read_groks(grok_glob)
+      @patterns    ||= Hash[pile.patterns.map {|name,match_expr| [name, (pile.compile("%{#{name}}"); Regexp.new(pile.expanded_pattern))]}]
       yield self if block_given?
       self
     end
     def read_samples(glob=sample_glob)
-      Dir.glob(glob).map {|f| File.read(f,nil,encoding: Encoding::BINARY) }
-    end
-    def read_groks(glob=grok_glob)
-      Dir.glob(glob).flat_map do |filename|
-        File.readlines(filename).map do |line|
-          line.chomp!
-          name,pattern = line.split(%r{\s+},2)
-          g=Grok.new
-          g.add_pattern(name,pattern)
-          g.compile("%{#{name}}")
-          g
-        end
+      data=Dir.glob(glob).map {|f| File.read(f,nil,encoding: Encoding::BINARY) }
+      if block_given? 
+        data.flat_map {|d| yield(d)}
+      else
+        data
       end
     end
-    def matchers
-      @matchers ||= patterns.map do |pattern|
-        case pattern
-        when String
-          Regexp.new(pattern)
-        when Regexp
-          pattern
-        when Grok
-          pattern
-        else
-          nil
-        end
-      end.reject(&:nil?)
+    def read_groks(glob=grok_glob)
+      pile=Grok.new
+      Dir.glob(glob).flat_map do |filename|
+        pile.add_patterns_from_file(filename)
+      end
+      pile
     end
     def sample_strings
       @sample_strings ||= samples.map(&:to_s)
@@ -57,10 +46,9 @@ module Piglab
       format('|%s|', str[0..end_idx].bytes.map {|b| b.to_s(16)}.join(" "))
     end
     def longest_match(str)
-      valid=Hash[matchers.zip(matchers.map {|p| p.match(str) })]
+      valid=Hash[patterns.map {|name,re| [name,re.match(str)]}]
                 .reject {|k,v| v.nil? || v==false }
-      grok,grokmatch = valid.max_by {|k,v| v.end - v.start }
-      grokmatch
+      valid.max_by {|k,v| v.captures.reject(&:nil?).map(&:length)}
     end
   end
 end
